e837c8b0c6df125238c42a203f4eecc477a30759
Christian Steinhart <steinhart.christian@gmail.com>
2015-02-12 22:22:35 +0100

    Rename all temporary 'TestAddon*' to 'Salvager*'

--- Salvager/Salvager.lua
+++ Salvager/Salvager.lua
@@ -1,11 +1,7 @@
 local loot, ignored_loot = {}, {}
 local enabled = false
 
-function HelloWorld() 
-  print("Hello, World!"); 
-end
-
-function TestAddonEventHandler(self, event, ...)
+function SalvagerEventHandler(self, event, ...)
     msg = ...
 
     -- print(event);
@@ -15,7 +11,7 @@ function TestAddonEventHandler(self, event, ...)
             table.insert(loot, itemLink)
         end
     end
-    TestAddonScrollBar_Update()
+    SalvagerScrollBar_Update()
     -- print(#(loot))
 end
 
@@ -33,25 +29,25 @@ function print_table(t)
     end
 end
 
-SLASH_TESTADDON1 = '/lootlist'
-SLASH_TESTADDON2 = '/ll'
-function SlashCmdList.TESTADDON(msg, editbox)
+SLASH_SALVAGER1 = '/salvager'
+SLASH_SALVAGER2 = '/ll'
+function SlashCmdList.SALVAGER(msg, editbox)
     show_lootlist()
 end
 
 
 function clear_loot()
     wipe(loot)
-    TestAddonScrollBar_Update()
+    SalvagerScrollBar_Update()
 end
 
 function show_lootlist()
-    TestAddonScrollBar_Update()
-    TestAddonFrame:Show()
+    SalvagerScrollBar_Update()
+    SalvagerFrame:Show()
 end
 
 function hide_lootlist()
-    TestAddonFrame:Hide()
+    SalvagerFrame:Hide()
 end
 
 function remove_from_list(itemLink)
@@ -61,38 +57,38 @@ function remove_from_list(itemLink)
             break
         end
     end
-    TestAddonScrollBar_Update();
+    SalvagerScrollBar_Update();
 end
 
-function TestAddonScrollBar_Update()
+function SalvagerScrollBar_Update()
 --   print("sbupdate")
    local line; -- 1 through 5 of our window to scroll
    local lineplusoffset; -- an index into our data calculated from the scroll offset
-   FauxScrollFrame_Update(TestAddonScrollBar,#(loot),5,16);
+   FauxScrollFrame_Update(SalvagerScrollBar,#(loot),5,16);
    for line=1,5 do
-     lineplusoffset = line + FauxScrollFrame_GetOffset(TestAddonScrollBar);
+     lineplusoffset = line + FauxScrollFrame_GetOffset(SalvagerScrollBar);
      if loot[lineplusoffset] ~= nil then
-       getglobal("TestAddonEntry"..line).text:SetText(loot[lineplusoffset]);
-       getglobal("TestAddonEntry"..line).info:SetText(TestAddonGetItemInfoString(loot[lineplusoffset]));
-       getglobal("TestAddonEntry"..line):Show();
+       getglobal("SalvagerEntry"..line).text:SetText(loot[lineplusoffset]);
+       getglobal("SalvagerEntry"..line).info:SetText(SalvagerGetItemInfoString(loot[lineplusoffset]));
+       getglobal("SalvagerEntry"..line):Show();
      else
-       getglobal("TestAddonEntry"..line):Hide();
+       getglobal("SalvagerEntry"..line):Hide();
      end
    end
 end
 
-function TestAddonGetItemInfoString(itemLink)
+function SalvagerGetItemInfoString(itemLink)
     -- itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(itemLink)
     _, _, _, itemLevel, itemMinLevel = GetItemInfo(itemLink)
     count = GetItemCount(itemLink);
     return "Lvl: " .. itemMinLevel .. ",  iLvl: " .. itemLevel .. ",  Qty: " .. count
 end
 
-function TestAddonSellItem(itemLink)
+function SalvagerSellItem(itemLink)
     if GetItemCount(itemLink) > 0 then
         for bag = 0,4,1 do 
             for slot = 1, GetContainerNumSlots(bag), 1 do
-                if TestAddonItemEquals(GetContainerItemLink(bag,slot), itemLink, (slot == 1 and bag == 0)) then
+                if SalvagerItemEquals(GetContainerItemLink(bag,slot), itemLink, (slot == 1 and bag == 0)) then
                     UseContainerItem(bag,slot);
                 end
             end
@@ -101,7 +97,7 @@ function TestAddonSellItem(itemLink)
     remove_from_list(itemLink)
 end
 
-function TestAddonDestroyItem(itemLink)
+function SalvagerDestroyItem(itemLink)
     if true then return end
     if GetItemCount(itemLink) > 0 then
         for bag = 0,4,1 do 
@@ -117,7 +113,7 @@ function TestAddonDestroyItem(itemLink)
     remove_from_list(itemLink)
 end
 
-function TestAddonIgnoreItem(itemLink)
+function SalvagerIgnoreItem(itemLink)
     if tContains(ignored_loot, itemLink) == nil then 
         table.insert(ignored_loot, itemLink)
     end
@@ -126,39 +122,39 @@ function TestAddonIgnoreItem(itemLink)
 end
 
 
-function TestAddonHandleItem(itemLink, action)
+function SalvagerHandleItem(itemLink, action)
     if action == "remove" then
         remove_from_list(itemLink);
     elseif action == "sell" then 
-        TestAddonSellItem(itemLink);
+        SalvagerSellItem(itemLink);
     elseif action == "destroy" then
-        TestAddonDestroyItem(itemLink);
+        SalvagerDestroyItem(itemLink);
     elseif action == "ignore" then
-        TestAddonIgnoreItem(itemLink);
+        SalvagerIgnoreItem(itemLink);
     else
         print(itemLink)
     end
 end
 
-function TestAddonToggleSellButton(self, event, ...)
+function SalvagerToggleSellButton(self, event, ...)
     if     event == "MERCHANT_SHOW" then self:Enable()
     elseif event == "MERCHANT_CLOSED" then self:Disable()
     end
 end
 
-function TestAddonToggleRecord()
+function SalvagerToggleRecord()
     if enabled then
-        TestAddonFrameButtonToggle:SetText("Enable")
-        TestAddonFrame:UnregisterEvent("CHAT_MSG_LOOT")
+        SalvagerFrameButtonToggle:SetText("Enable")
+        SalvagerFrame:UnregisterEvent("CHAT_MSG_LOOT")
         enabled = false
     else
-        TestAddonFrameButtonToggle:SetText("Disable")
-        TestAddonFrame:RegisterEvent("CHAT_MSG_LOOT")
+        SalvagerFrameButtonToggle:SetText("Disable")
+        SalvagerFrame:RegisterEvent("CHAT_MSG_LOOT")
         enabled = true
     end
 end
 
-function TestAddonItemEquals(item1, item2, debug)
+function SalvagerItemEquals(item1, item2, debug)
     if item1 == nil or item2 == nil then return false end
     itemName1, itemLink1, itemRarity1, itemLevel1, itemMinLevel1, itemType1, itemSubType1, itemStackCount1, itemEquipLoc1, itemTexture1, itemSellPrice1 = GetItemInfo(item1)
     itemName2, itemLink2, itemRarity2, itemLevel2, itemMinLevel2, itemType2, itemSubType2, itemStackCount2, itemEquipLoc2, itemTexture2, itemSellPrice2 = GetItemInfo(item2)
